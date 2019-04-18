9ee03ed4068820aeda629f42c8b9433f1f869a76
VaJau2 <vadim206-1998@yandex.ru>
2019-04-18 09:38:55 +0500

    Add kursach program files

---
+++ MunipActivesMap/geoJSON/Reestr1.js
@@ -0,0 +1,81 @@
+var BuildReestrGeoJSON=
+  {
+  "type": "FeatureCollection",
+  "name": "Реестр Зданий",
+  "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  "features": [
+  { "type": "Feature",
+    "properties":
+    {
+    "Specific": "Помещение",
+    "RegNum": "KUS31-1",
+    "Name": "Квартира, литера А",
+    "Adress": "д. Новый мир, ул. Молодежная, д. 14",
+    "BalHolder": "АДМИНИСТРАЦИИЯ СЕЛЬСКОГО ПОСЕЛЕНИЯ НИКОЛАЕВСКИЙ СЕЛЬСОВЕТ МУНИЦИПАЛЬНОГО РАЙОНА  МИЯКИНСКИЙ  РАЙОН р. БАШКОРТОСТАН",
+    "BalHoldNum": "ЮЗ3100",
+    "Area": "58,7",
+    "FloorNum": "1",
+    },
+    "geometry":
+    { "type": "Point", "coordinates": [ 55.059084, 53.694503 ] } },
+
+  { "type": "Feature",
+    "properties":
+    {
+    "Specific": "Здание",
+    "RegNum": "KUS31-2",
+    "Name": "Сельский клуб, литера А",
+    "Adress": "с. Чураево, ул. Демская, д. 28",
+    "BalHolder": "МИЯКИНСКИЙ ЦЕНТРАЛЬНЫЙ ДВОРЕЦ КУЛЬТУРЫ МУНИЦИПАЛЬНОГО РАЙОНА МИЯКИНСКИЙ РАЙОН р. БАШКОРТОСТАН",
+    "BalHoldNum": "ЮЗ3107",
+    "Area": "251,7",
+    "FloorNum": "2",
+    },
+    "geometry":
+    { "type": "Point", "coordinates": [ 	55.581108, 55.626413 ] } },
+
+  { "type": "Feature",
+    "properties":
+    {
+    "Specific": "Здание",
+    "RegNum": "KUS31-42",
+    "Name": "Общежитие №4",
+    "Adress": "г. Уфа, ул. 8 марта, д. 8",
+    "BalHolder": "УГАТУ",
+    "BalHoldNum": "Ю42104",
+    "Area": "203,1",
+    "FloorNum": "4",
+    },
+    "geometry":
+    { "type": "Point", "coordinates": [ 55.974218, 54.735931 ] } },
+
+  { "type": "Feature",
+    "properties":
+    {
+    "Specific": "Здание",
+    "RegNum": "KUS31-44",
+    "Name": "Общежитие №9",
+    "Adress": "г. Уфа, ул. Мингажева, д. 158/1",
+    "BalHolder": "УГАТУ",
+    "BalHoldNum": "Ю42109",
+    "Area": "265,3",
+    "FloorNum": "9",
+    },
+    "geometry":
+    { "type": "Point", "coordinates": [ 55.972021, 54.734236	 ] } },
+
+  { "type": "Feature",
+    "properties":
+    {
+    "Specific": "Здание",
+    "RegNum": "KUS11-41",
+    "Name": "Администрация Советского района городского округа г. Уфа",
+    "Adress": "г. Уфа, ул. Революционная, д. 111",
+    "BalHolder": "Администрация Советского района городского округа г. Уфа",
+    "BalHoldNum": "Ю4221",
+    "Area": "321,2",
+    "FloorNum": "4",
+    },
+    "geometry":
+    { "type": "Point", "coordinates": [ 55.967064, 54.733885] } },
+  ]};
